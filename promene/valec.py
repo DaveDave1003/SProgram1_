b20fabbcbe543cdc0920500ccc9824aa11e8fcb0
DaveDave1003 <dave0703@seznam.cz>
2024-12-12 19:17:20 +0100

    Update valec.py

--- promene/valec.py
+++ promene/valec.py
@@ -6,7 +6,7 @@ polomer = int(input("Zadejte poloměr: "))
 
 
 #Výsledné hodnoty
-print (f"Povrch válce: {round (2 * math.pi * polomer * (polomer + vyska), 2)} cm2")
-print (f"Objem válce: {round (math.pi * polomer ** 2 * vyska, 2)} cm3")
+print (f"Povrch válce: {round (2 * math.pi * polomer * (polomer + vyska), 2)}")
+print (f"Objem válce: {round (math.pi * polomer ** 2 * vyska, 2)}")
 
 
