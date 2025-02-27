295680eb6e52de4d7c7e18e51852f6ca04b9a4cb
DaveDave1003 <dave0703@seznam.cz>
2025-02-27 11:58:49 +0100

    test seznam

---
+++ Seznam/seznam_.py
@@ -0,0 +1,14 @@
+import math
+
+cisla = [1, 2, 3, 4, 5]
+cosla = [8, 9, 10, 3]
+
+cisla [2] = 99
+cisla.append(100)
+
+cisla.extend(cosla)
+cisla.remove(10)
+
+print(cisla [2])
+print(cisla [5])
+print(cisla)
