0b992694a360a85f8bce13246971ad98f9c70dd2
DaveDave1003 <dave0703@seznam.cz>
2025-05-29 13:04:54 +0200

    opraveno_funguje

--- Rekurze/vyhledani_cisla.py
+++ Rekurze/vyhledani_cisla.py
@@ -11,22 +11,22 @@ def hledej_cislo(seznam, nejm,nejv,x):
             return stred
         #Hledane cislo je vetsi nez stred
         elif seznam[stred] < x:
-            nejm = stred + 1
+            return hledej_cislo(seznam, stred + 1, nejv, x)
         #Hledane cislo je jinak mensi nez stred
         else:
-            nejv = stred - 1
+            return hledej_cislo(seznam, nejm, stred - 1, x)
     #Cislo v seznamu nebylo
     else:
         return None
 
 seznam = [2, 3, 4, 10, 40]
-x = 10
 
-vysledek = hledej_cislo(seznam, 0, len(seznam)-1, 10)
+
+vysledek = hledej_cislo(seznam, 0, len(seznam)-1, 2)
 
 
 if vysledek != None:
-    print("Hledané číslo je v seznamu", vysledek)
+    print("Hledané číslo je v seznamu na indexu", vysledek)
 else:
     print("Hledané číslo v seznamu není")
 
