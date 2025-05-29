2930901cb5d881ae25abd057cc71edf7dd6d5625
DaveDave1003 <dave0703@seznam.cz>
2025-05-29 12:59:22 +0200

    Update vyhledani_cisla.py

--- Rekurze/vyhledani_cisla.py
+++ Rekurze/vyhledani_cisla.py
@@ -6,25 +6,26 @@ import random
 def hledej_cislo(seznam, nejm,nejv,x):
     if nejv >= nejm:
         stred = nejm + (nejv - nejm) //2
-
+        #Hledane cislo je stred
         if seznam[stred] == x:
             return stred
-    
-        elif seznam[stred] > x: 
-            return hledej_cislo(seznam, nejm, stred-1, x)
-    
+        #Hledane cislo je vetsi nez stred
+        elif seznam[stred] < x:
+            nejm = stred + 1
+        #Hledane cislo je jinak mensi nez stred
         else:
-            return hledej_cislo(seznam, nejm, stred+1, nejv, x)
+            nejv = stred - 1
+    #Cislo v seznamu nebylo
     else:
-        return -1
+        return None
 
 seznam = [2, 3, 4, 10, 40]
 x = 10
 
-vysledek = hledej_cislo(seznam, 0, len(seznam)-1,x)
+vysledek = hledej_cislo(seznam, 0, len(seznam)-1, 10)
 
 
-if vysledek != -1:
+if vysledek != None:
     print("Hledané číslo je v seznamu", vysledek)
 else:
     print("Hledané číslo v seznamu není")
