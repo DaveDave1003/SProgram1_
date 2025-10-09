cece9f70c503d1ef07ef8cb70f390ee7eb17e861
DaveDave1003 <dave0703@seznam.cz>
2025-10-09 13:03:29 +0200

    pokus

--- Sort/shell_sort_v2.py
+++ Sort/shell_sort_v2.py
@@ -1,30 +1,5 @@
 import random
 
-
-
-
-def shell_sort(arr):
-    n = len(arr)
-    gap = n // 2
-    slozitost = 0
-
-    while gap > 0:
-
-        for i in range(gap, n):
-            slozitost += 1
-            temp = arr[i]
-            j = i 
-
-            while j >= gap and arr[j- gap] > temp:
-                arr[j] = arr[j - gap]
-                j -= gap
-            arr[j] = temp
-        gap //= 2
-
-    return arr, slozitost
-
-
-
 def shell_sort(arr):
     n = len(arr)
     gap = 3 * n + 1
@@ -45,5 +20,5 @@ def shell_sort(arr):
 
     return arr, slozitost
 
-
-
+arr = [random.randint(1, 100) for i in range(100)]
+print(shell_sort(arr))
