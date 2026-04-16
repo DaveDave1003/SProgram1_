7b8f4673d7afcb7c492519e46e8164180ba6b7be
DaveDave1003 <dave0703@seznam.cz>
2026-04-16 11:53:57 +0200

    mat_moc

---
+++ TEST/maturita/mocniny.py
@@ -0,0 +1,17 @@
+import math
+
+#INPUT
+Zaklad = int(input("Zadej základ: "))
+Exponent = int(input("Zadej mocninu: "))
+
+# Výpočet
+X = 1
+for x in range(abs(Exponent)):
+    X *= Zaklad
+
+    # Pokud je exponent záporný, obrátíme výsledek
+if Zaklad < 0:
+    X = 1 / X
+
+# VÝSLEDEK
+print(f"Výsledek {Zaklad} ^ {Exponent} je {X}.")
