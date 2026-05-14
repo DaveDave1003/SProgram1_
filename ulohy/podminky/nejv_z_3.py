a4e660c25a3b6acc0946043a4f4c99463ee7aa04
DaveDave1003 <dave0703@seznam.cz>
2026-05-14 13:10:42 +0200

    Uprava

--- ulohy/podminky/nejv_z_3.py
+++ ulohy/podminky/nejv_z_3.py
@@ -8,7 +8,9 @@ Cislo_3= float(input("Zadej třetí číslo: "))
 #Výpočet hodnot
 if Cislo_1>=Cislo_2 and Cislo_1>=Cislo_3:
     print (f"Největší číslo: {Cislo_1}")
+
 elif Cislo_2>=Cislo_1 and Cislo_2>=Cislo_3:
     print (f"Největší číslo: {Cislo_2}")
+
 elif Cislo_3>=Cislo_1 and Cislo_3>=Cislo_2:
     print (f"Největší číslo: {Cislo_3}")
---
+++ ulohy/podminky/porovnani.py
@@ -0,0 +1,19 @@
+def porovnej(c1,c2):
+    if c1 == c2:
+        return "Čísla jsou stejná"
+    
+    if c1 > c2:
+        return "První číslo je větší"
+
+    if c1 < c2:
+        return "Druhé číslo je větší"
+
+
+
+
+
+
+
+print(porovnej(7,6))
+print(porovnej(6,7))
+print(porovnej(7,7))
