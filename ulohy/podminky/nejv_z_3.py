54ee0f0353a1a14690916add42cb51f5a05acd21
DaveDave1003 <dave0703@seznam.cz>
2026-05-14 13:16:33 +0200

    oprava_z3

--- ulohy/podminky/nejv_z_3.py
+++ ulohy/podminky/nejv_z_3.py
@@ -1,16 +1,25 @@
 import math
 
+def porovnej_3 (Cislo_1, Cislo_2, Cislo_3):
+
+    #Výpočet hodnot
+    if Cislo_1>=Cislo_2 and Cislo_1>=Cislo_3:
+        return (f"Největší číslo: {Cislo_1}")
+
+    elif Cislo_2>=Cislo_1 and Cislo_2>=Cislo_3:
+        return (f"Největší číslo: {Cislo_2}")
+
+    elif Cislo_3>=Cislo_1 and Cislo_3>=Cislo_2:
+        return (f"Největší číslo: {Cislo_3}")
+
+
+
+
+
 #Zadej 3 libovolné hodnoty
-Cislo_1= float(input("Zadej první číslo: "))
-Cislo_2= float(input("Zadej druhé číslo: "))
-Cislo_3= float(input("Zadej třetí číslo: "))
+Cislo_1= int(input("Zadej první číslo: "))
+Cislo_2= int(input("Zadej druhé číslo: "))
+Cislo_3= int(input("Zadej třetí číslo: "))
 
-#Výpočet hodnot
-if Cislo_1>=Cislo_2 and Cislo_1>=Cislo_3:
-    print (f"Největší číslo: {Cislo_1}")
 
-elif Cislo_2>=Cislo_1 and Cislo_2>=Cislo_3:
-    print (f"Největší číslo: {Cislo_2}")
-
-elif Cislo_3>=Cislo_1 and Cislo_3>=Cislo_2:
-    print (f"Největší číslo: {Cislo_3}")
+print(porovnej_3(Cislo_1, Cislo_2, Cislo_3))
